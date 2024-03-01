67922fc3fb40a1f001d3ee8750145238b73c0c1f
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-01 09:31:15 +0700

    Initial Spring Code
    start.spring.io

---
+++ src/main/java/com/andriwicaksono/restapimavenproject/RestApiMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.andriwicaksono.restapimavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMavenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andriwicaksono/restapimavenproject/RestApiMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriwicaksono.restapimavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
