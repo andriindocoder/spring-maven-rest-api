b16adaedcbe373d030de0021c6e70dd8fd7a8140
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-05 21:55:17 +0700

    Update AuthController

--- src/main/java/com/andriwicaksono/restapimavenproject/controller/AuthController.java
+++ src/main/java/com/andriwicaksono/restapimavenproject/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.andriwicaksono.restapimavenproject.controller;
 
+import com.andriwicaksono.restapimavenproject.entity.User;
 import com.andriwicaksono.restapimavenproject.model.LoginUserRequest;
 import com.andriwicaksono.restapimavenproject.model.TokenResponse;
 import com.andriwicaksono.restapimavenproject.model.WebResponse;
 import com.andriwicaksono.restapimavenproject.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,15 @@ public class AuthController {
     public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
         TokenResponse tokenResponse = authService.login(request);
         return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user) {
+        authService.logout(user);
+        return WebResponse.<String>builder().data("OK").build();
     }
 
 }
