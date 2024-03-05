1cbd3a0f0129de3d6aeff50122cc0984820abf6d
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-05 19:41:53 +0700

    Update UserController and UserService

--- src/main/java/com/andriwicaksono/restapimavenproject/controller/UserController.java
+++ src/main/java/com/andriwicaksono/restapimavenproject/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.andriwicaksono.restapimavenproject.controller;
 
+import com.andriwicaksono.restapimavenproject.entity.User;
 import com.andriwicaksono.restapimavenproject.model.RegisterUserRequest;
+import com.andriwicaksono.restapimavenproject.model.UserResponse;
 import com.andriwicaksono.restapimavenproject.model.WebResponse;
 import com.andriwicaksono.restapimavenproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,15 @@ public class UserController {
     public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
         userService.register(request);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(User user) {
+        UserResponse userResponse = userService.get(user);
+            return WebResponse.<UserResponse>builder().data(userResponse).build();
     }
 
 }
--- src/main/java/com/andriwicaksono/restapimavenproject/service/UserService.java
+++ src/main/java/com/andriwicaksono/restapimavenproject/service/UserService.java
@@ -2,6 +2,7 @@ package com.andriwicaksono.restapimavenproject.service;
 
 import com.andriwicaksono.restapimavenproject.entity.User;
 import com.andriwicaksono.restapimavenproject.model.RegisterUserRequest;
+import com.andriwicaksono.restapimavenproject.model.UserResponse;
 import com.andriwicaksono.restapimavenproject.repository.UserRepository;
 import com.andriwicaksono.restapimavenproject.security.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,12 @@ public class UserService {
         user.setName(request.getName());
 
         userRepository.save(user);
+    }
+
+    public UserResponse get(User user) {
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .name(user.getName())
+                .build();
     }
 }
