94e792634473121f148c58b6f3f44b24774d4b53
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-05 19:12:36 +0700

    Add controller AuthController

---
+++ src/main/java/com/andriwicaksono/restapimavenproject/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.andriwicaksono.restapimavenproject.controller;
+
+import com.andriwicaksono.restapimavenproject.model.LoginUserRequest;
+import com.andriwicaksono.restapimavenproject.model.TokenResponse;
+import com.andriwicaksono.restapimavenproject.model.WebResponse;
+import com.andriwicaksono.restapimavenproject.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(
+            path = "/api/auth/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+        TokenResponse tokenResponse = authService.login(request);
+        return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+
+}
