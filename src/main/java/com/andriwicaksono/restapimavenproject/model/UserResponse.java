220f4b4c937eb84e4a777080a929ab2da03476a3
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-05 19:37:47 +0700

    Create model UserResponse

---
+++ src/main/java/com/andriwicaksono/restapimavenproject/model/UserResponse.java
@@ -0,0 +1,18 @@
+package com.andriwicaksono.restapimavenproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String username;
+
+    private String name;
+
+}
