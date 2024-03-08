f2e9e8943868e8e09f82d87000ef45fff7d51a2f
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-03-08 11:04:40 +0700

    Adding Contacts Entity, Use Actuator, Use Manual.http

--- database.sql
+++ database.sql
@@ -12,3 +12,17 @@ CREATE TABLE users
     PRIMARY KEY (username),
     UNIQUE (token)
 ) ENGINE InnoDB;
+
+CREATE TABLE contacts
+(
+    id                  VARCHAR(100) NOT NULL,
+    username            VARCHAR(100) NOT NULL,
+    first_name          VARCHAR(100) NOT NULL,
+    last_name           VARCHAR(100),
+    phone               VARCHAR(100),
+    email               VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_users_contacts (username) REFERENCES users(username)
+) ENGINE InnoDB;
+
+SELECT * FROM contacts;
---
+++ src/main/java/com/andriwicaksono/restapimavenproject/entity/Contact.java
@@ -0,0 +1,34 @@
+package com.andriwicaksono.restapimavenproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String Id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+}
